1907debfde4c464fc6e8f82aa9517581462fe030
Godswill Utong <godswillutong@gmail.com>
2025-03-06 10:52:41 +0100

    resolved Vercel deployment issue

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,16 +1,16 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+// import { createBrowserClient  } from '@supabase/ssr';
+// import { cookies } from 'next/headers';
+// import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get('code');
+// export async function GET(request: Request) {
+//   const requestUrl = new URL(request.url);
+//   const code = requestUrl.searchParams.get('code');
 
-  if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
-    await supabase.auth.exchangeCodeForSession(code);
-  }
+//   if (code) {
+//     const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,{ cookies });
+//     await supabase.auth.exchangeCodeForSession(code);
+//   }
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL('/', requestUrl.origin));
-} 
+//   // URL to redirect to after sign in process completes
+//   return NextResponse.redirect(new URL('/', requestUrl.origin));
+// } 
